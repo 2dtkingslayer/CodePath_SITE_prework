5ecc9aa40c641492a0e076e1da4f4db7fd628ce5
Glitch (glitch-hello-website) <none>
2022-03-30 01:34:44 +0000

    👦🏹 Checkpoint ./style.css:43025245/101 ./index.html:43025245/21 ./script.js:43025245/73

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // global constants
-const clueHoldTime = 1000; //how long to hold each clue's light/sound
+const clueHoldTime = 500; //how long to hold each clue's light/sound
 const cluePauseTime = 333; //how long to pause in between clues
-const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+const nextClueWaitTime = 200; //how long to wait before starting playback of the clue sequence
 
 //Global Variables
-var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var pattern = [1, 1, 2, 1, 4, 3, 1, 1, 2, 1, 5, 4, 1, 1, 8, 6, 4, 3, 2, 13, 6, 4, 5, 4];
 var progress = 0; 
 var gamePlaying = false;
 var tonePlaying = false;
